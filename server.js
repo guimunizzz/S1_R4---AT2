8e090236b758ebc874bebbd24d253ae119273916
guimunizzz <guilherme.muniz5@hotmail.com>
2025-10-02 11:01:04 -0300

    add exercise 2

--- server.js
+++ server.js
@@ -5,12 +5,38 @@ const PORT = 8081;
 // Middleware para utilização de dados JSON
 app.use(express.json()); // para trabalhar com json no express é necessario essa expressão
 
-app.post('/mensagem', (req,res) => {
+async function validaNumeros(pNumUm, pNumDois, pNumTres) {
     try {
-        const {nome, idade, timeFavorito } = req.body
-        res.send(`Ola ${nome}!, você tem ${idade} anos e torce para o ${timeFavorito}`)
+        if (isNaN(pNumUm) || isNaN(pNumDois) || isNaN(pNumTres)) {
+            throw new Error('Os valores digitados são inválidos');
+        } 
+        const numero1 = parseFloat(pNumUm);
+        const numero2 = parseFloat(pNumDois);
+        const numero3 = parseFloat(pNumTres)
+
+        return {numero1, numero2, numero3};
+
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+async function soma(pNumUm, pNumDois, pNumTres) {
+    try {
+        const {numero1, numero2, numero3} = await validaNumeros(pNumUm, pNumDois, pNumTres);
+        return numero1 + numero2 + numero3
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+app.post('/soma', async (req,res) => {
+    try {
+        const { numeroUm, numeroDois, numeroTres } = req.body
         
-        res.status(201).json({message: 'Dados recebidos com sucesso no servidor.'})
+        const resultadoSoma = await soma(numeroUm, numeroDois, numeroTres);
+
+        res.status(201).json({resultado: `O resultado da soma é ${resultadoSoma}`})
     } catch (error) {
         res.status(500).json({message: `Ocorreu um erro ao processar a requisição`, errorMessage: error.message});
     }
